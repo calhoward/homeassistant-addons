f7557ca3742135f9e472d4d5fd8627379e5d2b8b
Cal Howard <cal@calhoward.com>
2024-09-25 20:56:51 -0400

    modified:   mopidy/Dockerfile 	modified:   mopidy/config.yaml 	modified:   mopidy/run.sh

--- mopidy/run.sh
+++ mopidy/run.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/with-contenv bashio
 
-# Start Mopidy
-mopidy --config /var/lib/mopidy/.config/mopidy/mopidy.conf
+# Set up Mopidy configuration and data directories
+export XDG_CONFIG_HOME="/var/lib/mopidy/.config"
+export XDG_DATA_HOME="/var/lib/mopidy/.local/share"
+
+# Run Mopidy
+exec mopidy --config /var/lib/mopidy/.config/mopidy/mopidy.conf
