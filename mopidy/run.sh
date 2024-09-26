01dff27685513ce4191df6b402c07a13358bbe43
Cal Howard <cal@calhoward.com>
2024-09-25 20:48:55 -0400

    modified:   mopidy/Dockerfile 	modified:   mopidy/config.yaml 	modified:   mopidy/run.sh

--- mopidy/run.sh
+++ mopidy/run.sh
@@ -1,4 +1,24 @@
-#!/usr/bin/with-contenv bashio
+#!/bin/bash
 
-# Start Mopidy
-mopidy --config /var/lib/mopidy/.config/mopidy/mopidy.conf
+# Create Mopidy configuration if not exists
+mkdir -p /root/.config/mopidy
+cat << EOF > /root/.config/mopidy/mopidy.conf
+[core]
+cache_dir = /root/.cache/mopidy
+data_dir = /root/.local/share/mopidy
+config_dir = /root/.config/mopidy
+
+[http]
+hostname = 0.0.0.0
+port = ${INGRESS_PORT:-6680}
+default_app = iris
+
+[iris]
+enabled = true
+
+[audio]
+output = autoaudiosink
+EOF
+
+# Run Mopidy
+exec mopidy --config /root/.config/mopidy/mopidy.conf
