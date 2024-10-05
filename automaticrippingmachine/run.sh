bbe03a16479670ad4228a363678ce20910fd175b
Cal Howard <github@calhoward.com>
2024-10-05 12:26:56 -0400

    Delete automaticrippingmachine/run.sh

--- automaticrippingmachine/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# Export necessary environment variables if needed
-# Example:
-# export ARM_CONFIG_PATH=/config/arm_config.yaml
-
-# Start Automatic Ripping Machine
-exec /usr/local/bin/arm "$@"
